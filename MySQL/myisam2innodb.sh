8fbd11fd8c9771e7182ba20b0cc42bd838371f6a
Pascal A <pascalito@gmail.com>
2012-01-20 15:08:21 +0100

    convertion en bg commenté

--- MySQL/myisam2innodb.sh
+++ MySQL/myisam2innodb.sh
@@ -2,6 +2,8 @@
 
 for TABLE in `mysql --skip-column-names -B -e "SELECT table_schema, table_name FROM information_schema.tables WHERE table_schema NOT IN ('performance_schema','information_schema','mysql') AND engine = 'MyISAM' ORDER BY table_schema;" | awk '{print $1"."$2}'`; do
     mysql -e "ALTER TABLE $TABLE ENGINE=InnoDB;"
+    # Mode bourin ;)
+    #mysql -e "ALTER TABLE $TABLE ENGINE=InnoDB;" &
     echo -ne "."
 done
 echo -ne "\r\n"
