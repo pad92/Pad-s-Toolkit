08aea6b6376783bf3ecd5d9003bf936d2c85c55f
pad92 <pascalito@gmail.com>
2012-01-23 14:13:33 +0100

    add cnf auth file

--- MySQL/backup.sh
+++ MySQL/backup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 BCKDIR='/var/backup/mysql'
+MYCNF='/etc/mysql/debian.cnf'
 
 DATE=$(date '+%Y%m%d')
 DATEOLD=$(date '+%Y%m%d –date ’1 weeks ago’')
@@ -14,7 +15,7 @@ for BDD in `mysql --skip-column-names -B -e "SHOW databases;" | egrep -v "^infor
     mysql --skip-column-names -B -e "SHOW CREATE DATABASE $BDD;" | cut -d" " -f2- > $DST/create-$BDD.sql
     for TABLE in `mysql --skip-column-names -B $BDD -e "SHOW TABLES;" | grep -v slow_log | grep -v general_log`; do
         mkdir $DST/$BDD 2>/dev/null 1>&2
-        mysqldump --opt $BDD $TABLE > $DST/$BDD/$TABLE.sql 
+        mysqldump --defaults-file=$MYCNF --opt $BDD $TABLE > $DST/$BDD/$TABLE.sql
         echo -ne "."
     done
     gzip $DST/$BDD/*.sql &
