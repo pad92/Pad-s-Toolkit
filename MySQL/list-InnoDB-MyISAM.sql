705e149418348da009f61f7a5fdce44c553accce
Pascal A <pascalito@gmail.com>
2012-01-24 19:56:05 +0100

    mise en forme

--- MySQL/list-InnoDB-MyISAM.sql
+++ MySQL/list-InnoDB-MyISAM.sql
@@ -1,5 +1,5 @@
 SELECT table_schema, table_name, engine
-    FROM information_schema.tables
-    WHERE table_schema NOT IN ('mysql','performance_schema','information_schema')
-        AND engine != 'NULL'
-    ORDER BY table_schema;
+FROM information_schema.tables
+WHERE table_schema NOT IN ('mysql','performance_schema','information_schema')
+AND engine != 'NULL'
+ORDER BY table_schema;
