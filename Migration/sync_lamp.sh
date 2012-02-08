b5c7d1542600e2abe6b9944f94c772052ca1f273
Pascal A <pascalito@gmail.com>
2012-02-08 12:50:51 +0100

    add sync for all bdd/websites

--- Migration/sync_lamp.sh
+++ Migration/sync_lamp.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env sh
 
+######################
+#### ONFIGURATION ####
+######################
+
 REMOTE='serveur.prod.local'
 
+## pour synchroniser quelque dossier :
 DIRS="/var/www/site1/
 /var/www/site2/
 /var/www/site3/"
 
+## pour synchroniser tous les DocumentRoots :
+# DIRS=$(grep DocumentRoot /etc/apache2/sites-enabled/* | awk '{print $3}' | sed 's!/$!!' | sed 's!$!/!')
+
+## pour synchroniser quelque BDD :
 BDDS="bdd1
 bdd2
 bdd3"
+
+## pour synchroniser toutes les BDD (sauf les BDD systeme)
+#BDDS=$("show databases;" | egrep -v "information_schema|performance_schema|lost\+found|mysql")
+
+##############
+#### CORE ####
+##############
 
 echo "$(date '+%D %H:%M:%S') * rsync"
 for DIR in $DIRS
@@ -23,12 +39,13 @@ done
 echo "$(date '+%D %H:%M:%S') * bbd"
 for BDD in $BDDS
 do
-    echo "$(date '+%D %H:%M:%S') -- $BDD : Drop"
+    echo  -n "$(date '+%D %H:%M:%S') -- $BDD : drop"
     mysql -B -e "DROP DATABASE IF EXISTS \`$BDD\`;"
-    echo "$(date '+%D %H:%M:%S') -- $BDD : Create"
+    echo -n ", create"
     mysql -h $REMOTE --skip-column-names -B -e "SHOW CREATE DATABASE \`$BDD\`\G" | grep ^CREATE | mysql
-    echo "$(date '+%D %H:%M:%S') -- $BDD : Import"
+    echo -n ", import"
     mysqldump -h $REMOTE $BDD | mysql $BDD
+    echo -n ".\r\n"
 done
 
 
