2d8a22bcad6bd959a76c32dfc9b085eee6da949c
pad92 <pascalito@gmail.com>
2012-05-21 17:29:16 +0200

    5min for update/select, 1min for sleep

--- MySQL/krequete.sh
+++ MySQL/krequete.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-for requete in $(mysql INFORMATION_SCHEMA -B -e'select id from PROCESSLIST where INFO like "select%" and TIME >= "200"'| grep -v id); do
+for requete in $(mysql INFORMATION_SCHEMA -B -e'select id from PROCESSLIST where INFO like "select%" and TIME >= "300"'| grep -v id); do
 	/usr/bin/mysqladmin kill $requete
 	echo -ne "s"
 done
 
-for requete in $(mysql INFORMATION_SCHEMA -B -e'select id from PROCESSLIST where INFO like "update%" and TIME >= "200"'| grep -v id); do
+for requete in $(mysql INFORMATION_SCHEMA -B -e'select id from PROCESSLIST where INFO like "update%" and TIME >= "300"'| grep -v id); do
 	/usr/bin/mysqladmin kill $requete
 	echo -ne "u"
 done
