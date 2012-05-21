27bc3d0ae665c113cccf84f4ffc56bab670d29b0
Pascal A <pascal@tobor.local>
2012-05-21 17:27:11 +0200

    add MySQL Cleaner ! :)

---
+++ MySQL/krequete.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+for requete in $(mysql INFORMATION_SCHEMA -B -e'select id from PROCESSLIST where INFO like "select%" and TIME >= "200"'| grep -v id); do
+	/usr/bin/mysqladmin kill $requete
+	echo -ne "s"
+done
+
+for requete in $(mysql INFORMATION_SCHEMA -B -e'select id from PROCESSLIST where INFO like "update%" and TIME >= "200"'| grep -v id); do
+	/usr/bin/mysqladmin kill $requete
+	echo -ne "u"
+done
+
+for requete in $(mysql INFORMATION_SCHEMA -B -e'select id from PROCESSLIST where COMMAND like "Sleep" and TIME >= "60"'| grep -v id); do
+	/usr/bin/mysqladmin kill $requete
+	echo -ne "s"
+done
