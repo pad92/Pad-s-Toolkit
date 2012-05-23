974e75c7f7b6d4953cfed98e6fdd19261952a3ac
pad92 <pascalito@gmail.com>
2012-05-23 11:22:04 +0200

    Setup OSx scripts

---
+++ OSx/chromium.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# Install|Update Chromium Web browser on Mac OS
+
+CHROMIUM_URL="http://commondatastorage.googleapis.com/chromium-browser-continuous/index.html?path=Mac"
+CHROMIUM_URL2="http://commondatastorage.googleapis.com/chromium-browser-continuous/Mac"
+CHROMIUM_INSTALL_PATH="/Applications/"
+CHROMIUM_CURRENT_VERSION_FILE="$CHROMIUM_INSTALL_PATH/Chromium.app/Contents/Info.plist"
+CHROMIUM_CURRENT_VERSION=`defaults read /Applications/Chromium.app/Contents/Info SVNRevision`
+
+echo "Chromium version installed: $CHROMIUM_CURRENT_VERSION"
+echo "Checking for update: $CHROMIUM_URL2/LAST_CHANGE"
+CHROMIUM_LATEST_VERSION=$(curl -s -S $CHROMIUM_URL2/LAST_CHANGE)
+echo "Latest Chromium version   :  $CHROMIUM_LATEST_VERSION"
+
+if [ "$CHROMIUM_CURRENT_VERSION" == "" ] || [ $CHROMIUM_CURRENT_VERSION -lt $CHROMIUM_LATEST_VERSION ]; then
+   echo "Latest version of Chromium will be installed"
+   echo "Checking if Chromium is running"
+   if [ `ps -A | grep Chromium | wc -l` != 1 ]; then
+      echo "ERROR : Chromium must be closed before install new version"
+      exit 1
+   fi
+
+   echo "Downloading Chromium version $CHROMIUM_LATEST_VERSION"
+   echo "URL = $CHROMIUM_URL2/$CHROMIUM_LATEST_VERSION/chrome-mac.zip"
+   curl -L $CHROMIUM_URL2/$CHROMIUM_LATEST_VERSION/chrome-mac.zip > /tmp/chrome-mac.zip
+   cd /tmp
+   unzip chrome-mac.zip
+   rm -rf $CHROMIUM_INSTALL_PATH/Chromium.app
+   cp -R chrome-mac/Chromium.app $CHROMIUM_INSTALL_PATH
+   rm -Rf /tmp/chrome-mac*
+   echo "Chromium version $CHROMIUM_LATEST_VERSION installed"
+else
+   echo "No update available, you already have the latest version"
+fi
