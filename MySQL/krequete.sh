195df3dc40b4c67d6a3070862285e4e756bc7c75
pad92 <pascalito@gmail.com>
2012-05-25 15:39:12 +0200

    display query killed

--- MySQL/krequete.sh
+++ MySQL/krequete.sh
@@ -1,16 +1,33 @@
-#!env bash
+#!/usr/bin/env bash
 
-for QUERY in $(mysql INFORMATION_SCHEMA --skip-column-names -B -e'select id from PROCESSLIST where INFO like "select%" and TIME >= "300"'); do
-	mysqladmin kill $QUERY
-	echo -ne "s"
+MYCNF='/etc/mysql/debian.cnf'
+BIN_DEPS='mysql mysqladmin'
+
+for BIN in $BIN_DEPS; do
+   which $BIN 1>/dev/null 2>&1
+   if [ $? -ne 0 ]; then
+      echo "Error: Required file could not be found: $BIN"
+      exit 1
+   fi
 done
 
-for QUERY in $(mysql INFORMATION_SCHEMA --skip-column-names -B -e'select id from PROCESSLIST where INFO like "update%" and TIME >= "300"'); do
-	mysqladmin kill $QUERY
-	echo -ne "u"
+# Kill select >= 5min
+for QUERY_ID in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e 'SELECT id FROM information_schema.processlist WHERE INFO LIKE "SELECT%" and TIME >= "300"'); do
+   echo "* kill query $QUERY_ID !"
+   mysql  --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID"
+   mysqladmin kill $QUERY_ID
 done
 
-for QUERY in $(mysql INFORMATION_SCHEMA --skip-column-names -B -e'select id from PROCESSLIST where COMMAND like "Sleep" and TIME >= "60"'); do
-	mysqladmin kill $QUERY
-	echo -ne "s"
+# Kill update >= 5min
+for QUERY_ID in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e 'SELECT id FROM information_schema.processlist WHERE INFO LIKE "update%" and TIME >= "300"'); do
+   echo "* kill query $QUERY_ID !"
+   mysql  --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID"
+   mysqladmin kill $QUERY_ID
+done
+
+# Kill sleep >= 1min
+for QUERY_ID in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e 'SELECT id FROM information_schema.processlist WHERE COMMAND LIKE "Sleep" and TIME >= "60"'); do
+   echo "* kill query $QUERY_ID !"
+   mysql  --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID"
+   mysqladmin kill $QUERY_ID
 done
