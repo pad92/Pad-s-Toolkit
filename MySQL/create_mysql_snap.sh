49cb6959b3a71442c73c02274e5ddc3e7c75c0e9
pad92 <pascalito@gmail.com>
2012-06-12 14:35:10 +0200

    fix

--- MySQL/create_mysql_snap.sh
+++ MySQL/create_mysql_snap.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env sh
 
-
-### VARIABLES ###
-LV_MYSQL="/dev/system/var_lib_mysql"
-DATE=$(date '+%Y%m%d')
-
 ### TO DELETE
-echo "* check configuration file !!!"
-echo "* check if you have 1Go free in pv"
-echo "end delete this line ;)"
+echo "* check lv path"
+echo "* check if you have 3Go free in pv"
 exit 1
 
-
 ### CORE ###
-mysql -e "FLUSH TABLES WITH READ LOCK; SHOW MASTER STATUS;" && lvcreate -s -L 1G -n snap-$DATE $LV_MYSQL
+mysql -e "FLUSH TABLES WITH READ LOCK; SHOW MASTER STATUS; system lvcreate -s -L 3G -n snap /dev/system/var_lib_mysql; UNLOCK TABLES;"
 mysql -e "UNLOCK TABLES;"
 echo ""
 echo "To remove the snap :"
-echo "lvremove /dev/system/snap-$DATE"
+echo "lvremove /dev/system/snap"
