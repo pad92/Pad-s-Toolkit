30f9ccf5121617343cd5be1ca64cbccdfab329e0
pad92 <pascalito@gmail.com>
2012-06-15 12:29:54 +0200

    check if mysql is running :) ...

--- MySQL/krequete.sh
+++ MySQL/krequete.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-MYCNF='/etc/mysql/debian.cnf'
+MYCNF='/root/.my.cnf'
 BIN_DEPS='mysql mysqladmin'
 
 for BIN in $BIN_DEPS; do
@@ -17,22 +17,30 @@ f_log() {
     echo "$@"
 }
 
+mysqladmin --defaults-extra-file=$MYCNF status 1>/dev/null 2>&1
+if [ "$?" -ne "0" ]; then
+    f_log "MySQL status unavailable, exiting"
+    exit 1
+else
+    f_log $(mysqladmin --defaults-extra-file=$MYCNF status)
+fi
+
 # Kill select >= 5min
 for QUERY_ID in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e 'SELECT id FROM information_schema.processlist WHERE INFO LIKE "SELECT%" and TIME >= "300"'); do
     f_log "* kill SELECT query $QUERY_ID"
-    f_log $(mysql  --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID")
+    f_log $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID")
     mysqladmin kill $QUERY_ID
 done
 
 # Kill update >= 5min
 for QUERY_ID in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e 'SELECT id FROM information_schema.processlist WHERE INFO LIKE "update%" and TIME >= "300"'); do
     f_log "* kill UPDATE query $QUERY_ID"
-    f_log $(mysql  --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID")
-    mysqladmin kill $QUERY_ID
+    f_log $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID")
+    mysqladmin --defaults-extra-file=$MYCNF kill $QUERY_ID
 done
 
-# Kill sleep >= 1min
+# Kil sleep >= 1min
 for QUERY_ID in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e 'SELECT id FROM information_schema.processlist WHERE COMMAND LIKE "Sleep" and TIME >= "60"'); do
     f_log "* kill SLEEP query $QUERY_ID"
-    mysqladmin kill $QUERY_ID
+    mysqladmin --defaults-extra-file=$MYCNF kill $QUERY_ID
 done
