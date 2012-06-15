cb5c36e90a78e1ec5bd55d0a719557bdfb2aa0d4
pad92 <pascalito@gmail.com>
2012-06-15 12:02:49 +0200

    more verbose

--- MySQL/krequete.sh
+++ MySQL/krequete.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 MYCNF='/etc/mysql/debian.cnf'
 BIN_DEPS='mysql mysqladmin'
@@ -13,21 +13,21 @@ done
 
 # Kill select >= 5min
 for QUERY_ID in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e 'SELECT id FROM information_schema.processlist WHERE INFO LIKE "SELECT%" and TIME >= "300"'); do
-   echo "* kill query $QUERY_ID !"
+   echo "* kill SELECT query $QUERY_ID"
    mysql  --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID"
    mysqladmin kill $QUERY_ID
 done
 
 # Kill update >= 5min
 for QUERY_ID in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e 'SELECT id FROM information_schema.processlist WHERE INFO LIKE "update%" and TIME >= "300"'); do
-   echo "* kill query $QUERY_ID !"
+   echo "* kill UPDATE query $QUERY_ID"
    mysql  --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID"
    mysqladmin kill $QUERY_ID
 done
 
 # Kill sleep >= 1min
 for QUERY_ID in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e 'SELECT id FROM information_schema.processlist WHERE COMMAND LIKE "Sleep" and TIME >= "60"'); do
-   echo "* kill query $QUERY_ID !"
+   echo "* kill SLEEP query $QUERY_ID"
    mysql  --skip-column-names -B -e "SELECT Info FROM information_schema.processlist WHERE id = $QUERY_ID"
    mysqladmin kill $QUERY_ID
 done
