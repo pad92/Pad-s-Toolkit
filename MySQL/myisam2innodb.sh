927b95f560e132dd9caada10c4ca10957fee753f
Pascal A <pascalito@gmail.com>
2012-06-19 11:47:27 +0200

    fix

--- MySQL/myisam2innodb.sh
+++ MySQL/myisam2innodb.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
+MYCNF='/root/.my.cnf'
 BIN_DEPS='mysql'
 
 # CHECKS
 for BIN in $BIN_DEPS; do
-    which -s $BIN
+    which $BIN
     if [ $? -ne 0 ]; then
         echo "Error: Required file could not be found: $BIN"
         exit 1
@@ -12,10 +13,7 @@ for BIN in $BIN_DEPS; do
 done
 
 # CORE
-for TABLE in `mysql --skip-column-names -B -e "SELECT table_schema, table_name FROM information_schema.tables WHERE table_schema NOT IN ('performance_schema','information_schema','mysql') AND engine = 'MyISAM' ORDER BY table_schema;" | awk '{print $1"."$2}'`; do
-    mysql -e "ALTER TABLE $TABLE ENGINE=InnoDB;"
-    # Mode bourin ;)
-    #mysql -e "ALTER TABLE $TABLE ENGINE=InnoDB;" &
-    echo -ne "."
+for TABLE in $(mysql --defaults-extra-file=$MYCNF --skip-column-names -B -e "SELECT table_schema, table_name FROM information_schema.tables WHERE table_schema NOT IN ('performance_schema','information_schema','mysql') AND engine = 'MyISAM' ORDER BY table_schema;" | awk '{print $1"."$2}'); do
+    echo $TABLE
+    mysql --defaults-extra-file=$MYCNF -B -e "ALTER TABLE $TABLE ENGINE=InnoDB;"
 done
-echo -ne "\r\n"
