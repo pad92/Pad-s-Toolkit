b49e63b25a8825af6cad79821ef874c2a14c87e8
pad92 <pascalito@gmail.com>
2012-09-11 14:48:37 +0200

    add count

--- MySQL/list-InnoDB-MyISAM.sql
+++ MySQL/list-InnoDB-MyISAM.sql
@@ -1,5 +1,11 @@
+; list
 SELECT table_schema, table_name, engine
 FROM information_schema.tables
-WHERE table_schema NOT IN ('mysql','performance_schema','information_schema')
-AND engine != 'NULL'
+WHERE table_schema NOT IN ('mysql','performance_schema','information_schema') AND engine != 'NULL'
 ORDER BY table_schema;
+
+; count
+SELECT DISTINCT engine AS engine_name, count(engine) AS engine
+FROM information_schema.tables
+WHERE table_schema NOT IN ('mysql','performance_schema','information_schema') AND engine != 'NULL'
+GROUP BY engine;
