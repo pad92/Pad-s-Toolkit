e78e85ad28283f5b07de014976f19492f8bd962e
pad92 <pascalito@gmail.com>
2013-03-07 12:30:16 +0100

    add oldest query

--- MySQL/supervision.sql
+++ MySQL/supervision.sql
@@ -6,3 +6,14 @@ SELECT ( pl.connections / gv.max_connections ) * 100 AS percentage_used_connecti
     FROM
         ( SELECT COUNT(*) AS connections FROM information_schema.processlist ) AS pl,
         ( SELECT VARIABLE_VALUE AS max_connections FROM information_schema.global_variables WHERE variable_name = 'MAX_CONNECTIONS' ) AS gv
+
+# Count lock by database
+SELECT db,count(*)
+    FROM information_schema.processlist
+    WHERE state LIKE '%lock%';
+
+# Oldest query
+SELECT user,host,command,db,time
+    FROM information_schema.processlist
+    WHERE NOT (user  = 'system user' OR command = 'Binlog Dump')
+    ORDER BY time DESC LIMIT 1;
