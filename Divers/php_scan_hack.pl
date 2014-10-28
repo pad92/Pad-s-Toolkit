7e8bb760f3150d32fef528549d4fff257d0116e0
Pascal Armand <pascalito@gmail.com>
2014-10-28 12:24:33 +0100

    replace double quote into single in the array of patterns to find

--- Divers/php_scan_hack.pl
+++ Divers/php_scan_hack.pl
@@ -5,26 +5,26 @@
 use strict;
 
 my @patterns = (
-    "'BXcfTYewQ'",
-    "/etc/passwd",
-    "/etc/shadow",
-    "7b1dWyLJ0ih63T6P/6GG5RpgWpFPFW2dVkTF",
-    "9f681f3de8d1d1f5b469c2e0025b7fe3", # header add php SBA
-    "CRYPT.obfuscate",
-    "JGxsbGxsbGxsbGxsPSdiYXNlNjRfZGVjb2RlJzs=",
-    "PCEtLTlmNjgxZjNkZThkMWQxZj", # header SBA
-    "QAACOzh3b3cKDQoNKC0KDScAQkoAAEhDT",
-    "UnixCr3w",
-    "Web Shell",
-    "[S][h][a][u][n]",
-    "irc.allnetwork.org",
-    "milw0rm",
-    "str_rot13",
-    "tmpdir.md5", # content injecte SBA
-    "uggc://",
-    "base64_decode",
-    "eval",
-    "gzinflate",
+    'BXcfTYewQ',
+    '/etc/passwd',
+    '/etc/shadow',
+    '7b1dWyLJ0ih63T6P/6GG5RpgWpFPFW2dVkTF',
+    '9f681f3de8d1d1f5b469c2e0025b7fe3', # header add php SBA
+    'CRYPT.obfuscate',
+    'JGxsbGxsbGxsbGxsPSdiYXNlNjRfZGVjb2RlJzs=',
+    'PCEtLTlmNjgxZjNkZThkMWQxZj', # header SBA
+    'QAACOzh3b3cKDQoNKC0KDScAQkoAAEhDT',
+    'UnixCr3w',
+    'Web Shell',
+    '[S][h][a][u][n]',
+    'irc.allnetwork.org',
+    'milw0rm',
+    'str_rot13',
+    'tmpdir.md5', # content injecte SBA
+    'uggc://',
+    'base64_decode',
+    'eval',
+    'gzinflate',
     '\\x65\\x76\\x61\\x6C', #eval
     '\x62\x61\x73\x65\x36\x34\x5F\x64\x65\x63\x6F\x64\x65' # base64_decode
     '\x65\x76\x61\x6C\x28\x67\x7A\x69\x6E\x66\x6C\x61\x74\x65\x28\x62\x61\x73\x65\x36\x34\x5F\x64\x65\x63\x6F\x64\x65\x28', #eval(gzinflate(base64_decode
