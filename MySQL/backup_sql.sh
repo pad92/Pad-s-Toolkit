e44256a137db0b96925d76322763591b09dd1a0e
Pascal Armand <pascal.armand@cloud-temple.com>
2017-04-12 15:47:49 +0200

    update for with and without gtid replication

--- MySQL/backup_sql.sh
+++ MySQL/backup_sql.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 #
-# Reference : http://git.depad.fr/pascal/pad-s-toolkit/raw/master/MySQL/backup_sql.sh
+# Reference : https://git.depad.fr/depad/pad-s-toolkit/raw/master/MySQL/backup_sql.sh
+#
+# Install : wget https://git.depad.fr/depad/pad-s-toolkit/raw/master/MySQL/backup_sql.sh -O /usr/local/bin/backup_mysql.sh && chmod +x /usr/local/bin/backup_mysql.sh
 
 # === CONFIG ===
-BCKDIR='/var/backup/mysql'    # backup destination
-MYCNF='/etc/mysql/debian.cnf' # mysql account for dumps
-CNF='/etc/mysql/'             # mysql configuration
+BCKDIR='/var/backups/mysql'         # backup destination
+MYCNF='/etc/mysql/debian.cnf'       # mysql account for dumps
+CNF='/etc/mysql/'                   # mysql configuration
+#MYSQL_PARAM='--set-gtid-purged=OFF' # MySQL > 5.6
+MYSQL_PARAM=''                      # MySQL < 5.6
 
 BIN_DEPS='xz mysql mysqldump'
 DATE=$(date '+%Y.%m.%d')
@@ -19,7 +23,7 @@ for BIN in ${BIN_DEPS}; do
     if [ $? -ne 0 ]; then
         echo "Error: Required file could not be found: ${BIN}"
         exit 1
-    fi  
+    fi
 done
 
 if [ ! -d "${DST}" ];  then mkdir -p ${DST};   fi
@@ -28,7 +32,7 @@ if [ -d "${DSTOLD}" ]; then rm -fr  ${DSTOLD}; fi
 # === FUNCTION ===
 f_log() {
     logger "$0 - $@"
-}   
+}
 
 # === CORE ===
 f_log "** START **"
@@ -37,19 +41,19 @@ for BDD in `mysql --defaults-file=${MYCNF} --skip-column-names -B -e "SHOW datab
     f_log "* Processing BDD ${BDD}"
     mysql --defaults-file=${MYCNF} --skip-column-names -B -e "SHOW CREATE DATABASE \`${BDD}\`;" | awk -F"\t" '{ print $2 }' > ${DST}/${BDD}-create.sql
     f_log "  > Export 'SHOW CREATE TABLE'"
-    mysqldump --defaults-file=${MYCNF} --set-gtid-purged=OFF --routines --no-create-info --no-data --no-create-db --skip-opt ${BDD} > ${DST}/${BDD}-routines.sql
+    mysqldump --defaults-file=${MYCNF} ${MYSQL_PARAM} --routines --no-create-info --no-data --no-create-db --skip-opt ${BDD} > ${DST}/${BDD}-routines.sql
     f_log "  > Exports Routines"
     for TABLE in `mysql --defaults-file=${MYCNF} --skip-column-names -B ${BDD} -e "SHOW TABLES;" | grep -v slow_log | grep -v general_log`; do
         mkdir -p ${DST}/${BDD} 2>/dev/null 1>&2
         chown mysql:mysql ${DST}/${BDD}
         f_log "  ** Dump ${BDD}.${TABLE}"
-        mysqldump --defaults-file=${MYCNF} --set-gtid-purged=OFF --single-transaction ${BDD} ${TABLE} > "${DST}"/"${BDD}"/"${TABLE}".sql
+        mysqldump --defaults-file=${MYCNF} ${MYSQL_PARAM} --single-transaction ${BDD} ${TABLE} > "${DST}"/"${BDD}"/"${TABLE}".sql
         if [ -f "${DST}/${BDD}/${TABLE}.sql" ]; then
             f_log "  ** xz ${BDD}/${TABLE}.txt in background"
             xz ${DST}/${BDD}/${TABLE}.sql &
         else
             f_log "  ** WARNING : ${DST}/${BDD}/${TABLE}.sql not found"
-        fi  
+        fi
     done
 done
 f_log "** END **"
