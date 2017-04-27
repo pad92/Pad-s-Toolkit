7bb8b1e16f30db357edaa0b70abe3fef6d00dbf1
Pascal Armand <pascal.armand@cloud-temple.com>
2017-04-27 16:39:51 +0200

    update

--- Divers/inventaire.sh
+++ Divers/inventaire.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
 DIR='/tmp'
+LISTING=`grep '^172.21' /etc/hosts | awk '{print $NF}' | sort | uniq`
 
 DATE=`date +%Y%m%d`
-INV_FILE="inventaire.${DATE}.csv"
+INV_CSV="inventaire.${DATE}.csv"
 
 rm -fr "$DIR/*$DATE.*.inv"
 
-echo 'Hostname;Paquet;Version' > $INV_FILE
+echo 'Hostname;Paquet;Version' > $INV_CSV
 
-for i in $(grep '^172.21' /etc/hosts | awk '{print $NF}' | sort | uniq ); do
+for i in $LISTING ; do
     echo $i
     ssh $i 'rpm -qa --queryformat "%{NAME};%{VERSION}\n"'           1> $DIR/$i.$DATE.rpm.inv  2>/dev/null
     ssh $i "dpkg-query -W -f='\${binary:Package};\${Version}\n'"    1> $DIR/$i.$DATE.dpkg.inv 2>/dev/null
     for PACKAGE in $( cat $DIR/$i.$DATE.*.inv); do
-        echo "$i;$PACKAGE" >> $DIR/$INV_FILE
+        echo "$i;$PACKAGE" >> $DIR/$INV_CSV
     done
     rm -fr $DIR/$i.$DATE.rpm.inv $DIR/$i.$DATE.dpkg.inv
 done
 
-echo "done : $DIR/$INV_FILE"
+echo "done : $DIR/$INV_CSV"
