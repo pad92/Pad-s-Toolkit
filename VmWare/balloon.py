850c7ff3bdb615806af9439598775615dbf56a85
Pascal Armand <pad@intrinsec.com>
2017-07-28 15:29:53 +0200

    add vmware ps script

---
+++ VmWare/balloon.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# VMware vSphere Python SDK
+# Copyright (c) 2008-2013 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Python program to authenticate and print
+a friendly encouragement to joining the community!
+"""
+import atexit
+import argparse
+import getpass
+import ssl
+
+from pyVim import connect
+from pyVmomi import vmodl
+
+MAX_DEPTH = 10
+
+def get_args():
+    """Get command line args from the user.
+    """
+    parser = argparse.ArgumentParser(
+        description='Standard Arguments for talking to vCenter')
+
+    # because -h is reserved for 'help' we use -s for service
+    parser.add_argument('-s', '--host',
+                        required=False,
+                        action='store',
+                        help='vSphere service to connect to')
+
+    # because we want -p for password, we use -o for port
+    parser.add_argument('-o', '--port',
+                        type=int,
+                        default=443,
+                        action='store',
+                        help='Port to connect on')
+
+    parser.add_argument('-u', '--user',
+                        required=False,
+                        action='store',
+                        help='User name to use when connecting to host')
+
+    parser.add_argument('-p', '--password',
+                        required=False,
+                        action='store',
+                        help='Password to use when connecting to host')
+
+    args = parser.parse_args()
+
+    if not args.host:
+        args.host = raw_input('Enter vcenter host : ')
+    if not args.user:
+        args.user = raw_input('Enter username : ')
+    if not args.password:
+        args.password = getpass.getpass(
+                prompt='Enter password : ' ) 
+    return args
+
+def printvminfo(vm, depth=1):
+    """
+    Print information for a particular virtual machine or recurse into a folder
+    with depth protection
+    """
+
+    # if this is a group it will have children. if it does, recurse into them
+    # and then return
+    if hasattr(vm, 'childEntity'):
+        if depth > MAX_DEPTH:
+            return
+        vmlist = vm.childEntity
+        for child in vmlist:
+            printvminfo(child, depth+1)
+        return
+
+    summary = vm.summary
+    print(summary.config.name)
+
+def main():
+    """
+    Simple command-line program for listing the virtual machines on a system.
+    """
+
+    args = get_args()
+
+    try:
+	sslContext = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+	sslContext.verify_mode = ssl.CERT_NONE
+        service_instance = connect.SmartConnect(host=args.host,
+                                                user=args.user,
+                                                pwd=args.password,
+                                                port=int(args.port),
+						sslContext=sslContext)
+
+        atexit.register(connect.Disconnect, service_instance)
+    except vmodl.MethodFault as error:
+        print "Caught vmodl fault : " + error.msg
+        return -1
+
+    content = service_instance.RetrieveContent()
+    for child in content.rootFolder.childEntity:
+        if hasattr(child, 'vmFolder'):
+            datacenter = child
+            vmfolder = datacenter.vmFolder
+            vmlist = vmfolder.childEntity
+            for vm in vmlist:
+                printvminfo(vm)
+                balloonedMemory(vm)
+    return 0
+
+# Start program
+if __name__ == "__main__":
+    main()
