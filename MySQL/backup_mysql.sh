9ffee23a9c8d44b07b159d09dbaa09f06e3af2c9
Pascal <pascalito@gmail.com>
2018-05-19 12:10:08 +0200

    backup users

--- MySQL/backup_mysql.sh
+++ MySQL/backup_mysql.sh
@@ -39,7 +39,7 @@ f_log() {
 # === CORE ===
 f_log "** START **"
 tar cPJf ${DST}/etc_mysql.tar.xz ${CNF}/
-mysqlpump --exclude-databases=% --add-drop-user --users | xz > ${DST}/users.sql.xz
+mysqldump --defaults-file=${MYCNF} ${MYSQL_PARAM} --exclude-databases=% --add-drop-user --users | xz > ${DST}/users.sql.xz
 {
     for BDD in `mysql --defaults-file=${MYCNF} --skip-column-names -B -e "SHOW databases;" | egrep -v "^information_schema$|^performance_schema$"`; do
         f_log "* Processing BDD ${BDD}"
